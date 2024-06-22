cc71417db6d47878dd29415f0a933513db44dd2c
Joy Donatus <donatus.joy@outlook.com>
2024-06-22 09:30:25 +0100

    real commit

--- script.js
+++ script.js
@@ -1,33 +1,16 @@
-let span = document.getElementsByClassName('left');
-let product = document.getElementsByClassName('flip-card')
-let product_page = Math.ceil(product.length/4);
-let l = 0;
-let movePer = 25.34;
-let maxMove = 203;
-// mobile_view	
-let mob_view = window.matchMedia("(max-width: 768px)");
-if (mob_view.matches)
- {
-     movePer = 50.36;
-     maxMove = 504;
- }
-
-let right_mover = ()=>{
-    l = l + movePer;
-    if (product == 1){l = 0; }
-    for(const i of product)
-    {
-        if (l > maxMove){l = l - movePer;}
-        i.style.left = '-' + l + '%';
-    }
-
-}
-let left_mover = ()=>{
-    l = l - movePer;
-    if (l<=0){l = 0;}
-    for(const i of product){
-        if (product_page>1){
-            i.style.left = '-' + l + '%';
-        }
-    }
-}
+var swiper = new Swiper('.scrollImages', {
+    slidesPerView : 3,
+    spaceBetween: 20,
+    slidesPerGroup: 3,
+    loop: true,
+    centerSlider: "true",
+    fade: "true",
+    pagination:{
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation:{
+        nextEl : ".right",
+        prevEl: ".left"
+    },
+})
