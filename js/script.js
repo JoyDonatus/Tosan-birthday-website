a7f6421773943d5a238b43f4e4981b9b21e42ea4
Joy Donatus <donatus.joy@outlook.com>
2024-06-27 13:39:52 +0100

    yes

---
+++ js/script.js
@@ -0,0 +1,44 @@
+var swiper = new Swiper(".cover", {
+    slidesPerView: 1,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: true,
+    fade: true,
+    keyboard: {
+      enabled: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        1199: {
+          slidesPerView: 3,
+          slidesPerGroup: 3,
+        },
+        750:{
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+        },
+        0:{
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+        },
+      },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+//after spaceBetween; slidesPerGroup: 3, after loop: true, loopFillGroupWithBlank: true,
+//after pagination:  breakpoints: {
+      //769: {
+       // slidesPerView: 3,
+       // slidesPerGroup: 3,
+      //},
+   // },
+
+
+//centeredSlides: false,
+//grabCursor: true,
